54d94ee9308e44c2fd0f781759698235135f93ab
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2023-01-08 17:45:38 +0600

    Improved Age Calculation

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-const currYear: number = new Date().getFullYear();
-const age: number = currYear - 2006;
+import dayjs from "dayjs";
+const birthday = dayjs("2006-08-16");
+const age = dayjs().diff(birthday, "years");
 document.querySelector<HTMLSpanElement>("#age")!.innerHTML = age.toString();
 
 export {};
